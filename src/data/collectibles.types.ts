9238db84f06586dda4da17f3d582ef0f8a717770
uxuz <hello@uxuz.zip>
2026-02-03 21:57:48 +0100

    feat: collectibles data

---
+++ src/data/collectibles.types.ts
@@ -0,0 +1,17 @@
+export interface Collectible {
+  contractAddress: string;
+  deployedAt: string;
+  startingTokenId: number;
+  productId: string;
+  name: string;
+  description: string;
+  supply: number;
+  previewUrl: string;
+  backgroundUrl: string;
+  status: string;
+  creator: string;
+  sold: number;
+  price: 249 | 499 | 999 | 2499 | 4999 | 7499 | 9999 | 19999;
+  traits: string[];
+  tags: string[];
+}
