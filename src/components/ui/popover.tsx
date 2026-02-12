afafe71368a0d18672208ddc68ba93d0a933d18f
uxuz <hello@uxuz.zip>
2026-02-12 22:02:49 +0100

    fix: bg-background, in line color mix doesn't work with chrome

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -30,7 +30,7 @@ function PopoverContent({
         align={align}
         sideOffset={sideOffset}
         className={cn(
-          "text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 border-dim/10 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 origin-(--radix-popover-content-transform-origin) rounded-lg border bg-[color-mix(var(--color-dim)_10%,var(--color-background))] p-2 shadow-md outline-hidden",
+          "text-dim data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 border-dim/10 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-background z-50 origin-(--radix-popover-content-transform-origin) rounded-lg border p-2 shadow-md outline-hidden",
           className,
         )}
         {...props}
