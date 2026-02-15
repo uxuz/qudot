eb753d2d437f2ed66c00cb532d81a6c0578b450f
uxuz <hello@uxuz.zip>
2026-02-15 21:31:34 +0100

    feat: creator page

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,14 +1,16 @@
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
+import collectiblesData from "@/data/collectibles.json";
 import creatorData from "@/data/creators.json";
-import type { Creator } from "@/data/data.types";
+import type { Collectible, Creator } from "@/data/data.types";
 import { Avatar } from "@/components/custom/Avatar";
 
 interface PageProps {
   params: { username: string };
 }
 
+const collectibles = collectiblesData as Collectible[];
 const creators = creatorData as Creator[];
 
 export async function generateStaticParams() {
@@ -29,7 +31,7 @@ export async function generateMetadata({
   }
 
   return {
-    title: `${creator.displayName} (@${username})`,
+    title: `${creator.displayName} (@${creator.username})`,
     description: creator.description,
   };
 }
@@ -43,18 +45,48 @@ export default async function CollectiblePage({ params }: PageProps) {
     notFound();
   }
 
+  const creatorCollectibles = collectibles.filter(
+    (collectible) => collectible.creator === creator.username,
+  );
+
   return (
     <>
-      <div className="px-horizontal border-dim/10 space-y-3 border-b pt-12 pb-3">
-        <div className="flex items-end">
-          <Avatar name={username} size={72} />
+      <section className="px-horizontal border-dim/10 flex flex-col gap-3 border-b pt-12 pb-3 sm:flex-row sm:gap-x-6">
+        <div className="flex items-center gap-3">
+          <Avatar name={creator.username} size={128} />
         </div>
-        <div className="flex flex-col">
-          <span className="text-xl font-bold">{creator.displayName}</span>
-          <span className="text-dim">@{username}</span>
+        <div className="space-y-3 sm:self-center">
+          <div className="flex flex-col">
+            <span className="text-xl font-bold">{creator.displayName}</span>
+            <span className="text-dim">@{creator.username}</span>
+          </div>
+          <p>{creator.description}</p>
+          <div className="flex gap-3">
+            <div>
+              <span className="font-bold">{creatorCollectibles.length}</span>{" "}
+              <span className="text-dim">
+                {creatorCollectibles.length === 1
+                  ? "Collectible"
+                  : "Collectibles"}
+              </span>
+            </div>
+            <div>
+              <span className="font-bold">
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(
+                  creatorCollectibles.reduce(
+                    (total, item) => total + (item.sold * item.price) / 100,
+                    0,
+                  ),
+                )}
+              </span>{" "}
+              <span className="text-dim">Revenue</span>
+            </div>
+          </div>
         </div>
-        <p>{creator.description}</p>
-      </div>
+      </section>
     </>
   );
 }
