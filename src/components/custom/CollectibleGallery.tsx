2a307d1abb2fd0354053fa45cc3fcdd48e30aaeb
uxuz <hello@uxuz.zip>
2026-02-16 20:08:35 +0100

    refactor: collectible gallery styles

--- src/components/custom/CollectibleGallery.tsx
+++ src/components/custom/CollectibleGallery.tsx
@@ -1,26 +1,30 @@
-"use client";
-
 import type { Collectible } from "@/data/data.types";
 import { CollectibleCard } from "./CollectibleCard";
+import { cn } from "@/lib/utils";
 
-// TODO: This needs to be virtualized, as rendering over 7k collectibles kills navigation and performance in general
 export function CollectibleGallery({
   collectibles,
+  className,
+  ...props
 }: {
   collectibles: Collectible[];
-}) {
+} & React.ComponentProps<"div">) {
   return (
-    <div className="w-full">
-      <div className="px-horizontal grid grid-cols-2 gap-2 sm:grid-cols-3">
-        {collectibles.map((collectible) => {
-          return (
-            <CollectibleCard
-              key={collectible.productId}
-              collectible={collectible}
-            />
-          );
-        })}
-      </div>
+    <div
+      className={cn(
+        "px-horizontal grid grid-cols-2 gap-2 sm:grid-cols-3",
+        className,
+      )}
+      {...props}
+    >
+      {collectibles.map((collectible) => {
+        return (
+          <CollectibleCard
+            key={collectible.productId}
+            collectible={collectible}
+          />
+        );
+      })}
     </div>
   );
 }
