202c7a6367f5bec45f7968b24cb3d50ee7c82625
uxuz <hello@uxuz.zip>
2026-02-16 20:35:41 +0100

    feat: robots.txt and sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,38 @@
+import type { MetadataRoute } from "next";
+
+import collectiblesData from "@/data/collectibles.json";
+import creatorData from "@/data/creators.json";
+import type { Collectible, Creator } from "@/data/data.types";
+
+const collectibles = collectiblesData as Collectible[];
+const creators = creatorData as Creator[];
+
+export const baseUrl = "https://blurbury.com";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const creatorUrls: MetadataRoute.Sitemap = creators.map((creator) => ({
+    url: `${baseUrl}/${creator.username}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.7,
+  }));
+  const collectibleUrls: MetadataRoute.Sitemap = collectibles.map(
+    (collectible) => ({
+      url: `${baseUrl}/collectibles/${collectible.productId}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    }),
+  );
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    ...creatorUrls,
+    ...collectibleUrls,
+  ];
+}
