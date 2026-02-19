7c886af5b9e48924676d918e196321b4054433ae
uxuz <hello@uxuz.zip>
2026-02-19 19:42:09 +0100

    feat: basic creators page

--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -1,5 +1,25 @@
-import { collectibles, creators } from "@/data/data";
+import Link from "next/link";
 
+import { collectibles, creators } from "@/data/data";
+import { Avatar } from "@/components/custom/Avatar";
+
+// TODO: Actual styling, revenue, collectible amount and ability to sort by name, revenue and collectible amount
 export default function Creators() {
-  return <div>WIP</div>;
+  return (
+    <div className="px-horizontal grid gap-2 sm:grid-cols-2">
+      {creators.map((creator) => (
+        <Link
+          key={creator.username}
+          href={`/${creator.username.toLocaleLowerCase()}`}
+          className="flex gap-2"
+        >
+          <Avatar name={creator.username} />
+          <div>
+            <div className="font-bold">{creator.displayName}</div>
+            <div className="text-dim">{creator.username}</div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
