ba7b9f89d23b7eb0ad75a65720e04871be3198e7
uxuz <hello@uxuz.zip>
2026-02-19 19:23:14 +0100

    feat: add comments to proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,3 +1,7 @@
+// This is only used in development and will be removed in the final version
+// The entire application will essentially be completely static without any serverless edge functions
+// Lowercase redirects will be handled by a simple Cloudflare rule that will handle redirects to the lowercase version
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
