0ffeda229e9882c2d8d439aa3588edb30e4b3bc3
uxuz <hello@uxuz.zip>
2026-02-26 22:24:11 +0100

    refactor: rename the filename of the HomeCollectibleGallery component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { HomeCollectibleGallery } from "@/components/custom/CollectibleGalleryAll";
+import { HomeCollectibleGallery } from "@/components/custom/HomeCollectibleGallery";
 
 export default function Home() {
-  // Reduce the array to just 100 collectibles for development, since rendering 7k collectibles tanks the performance to hell
-  return <HomeCollectibleGallery />;
+  return;
+  <>
+    <HomeCollectibleGallery />;
+  </>;
 }
