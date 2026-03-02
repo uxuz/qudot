e3de5f4d65977b9ae2af84e7ebea0fcf5486c5fc
uxuz <hello@uxuz.zip>
2026-03-02 22:12:57 +0100

    feat: prototype hero section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,23 @@
 import { CollectiblesClient } from "@/app/(root)/CollectiblesClient";
+import { LucideArrowRight } from "@/components/icons/Lucide";
+import { LinkButton } from "@/components/shared/LinkButton";
 
 export default function Home() {
   return (
     <>
+      <section className="px-horizontal space-y-3 pt-12 pb-6">
+        <h1 className="text-xl font-bold tracking-tight text-balance">
+          Find every Reddit Collectible Avatar ever released in the shop.
+        </h1>
+        <p className="text-dim">
+          Free, static and open source. Looking for a specific creator or want
+          to search by display name? Head to the creators page to search and
+          explore every creator.
+        </p>
+        <LinkButton href="/creators">
+          Explore Creators <LucideArrowRight />
+        </LinkButton>
+      </section>
       <CollectiblesClient />
     </>
   );
--- src/components/icons/Lucide.tsx
+++ src/components/icons/Lucide.tsx
@@ -370,3 +370,25 @@ export function LucideArrowDownWideNarrow(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function LucideArrowRight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      {/* Icon from Lucide by Lucide Contributors - https://github.com/lucide-icons/lucide/blob/main/LICENSE */}
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M5 12h14m-7-7l7 7l-7 7"
+      />
+    </svg>
+  );
+}
